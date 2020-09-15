26b33798baab93699128afdd3c894543813bb742
Michael Kinful <epyon85@gmail.com>
2020-09-15 17:58:08 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+var log = document.getElementById("login");
+var reg = document.getElementById("register");
+var button = document.getElementById("btn");
+
+function login(){
+    log.style.left = "50px";
+    reg.style.left = "450px";
+    button.style.left = "0px";
+}
+
+function register(){
+    log.style.left = "-400px";
+    reg.style.left = "50px";
+    button.style.left = "110px";
+}
